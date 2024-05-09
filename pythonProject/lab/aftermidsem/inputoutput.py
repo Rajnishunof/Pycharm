53612a9a37b34c2fb18859b38ab70ca9a09be674
Raj <rsaitersoa@gmail.com>
2024-05-09 14:39:59 +0530

    firstclass after midsem

---
+++ pythonProject/lab/aftermidsem/inputoutput.py
@@ -0,0 +1,37 @@
+import sys
+# for line in sys.stdin:
+#     if line.rstrip()=='q':
+#         break
+#     print('Input ',line)
+#     # print('Input ',line.rstrip())
+# print("Exit")
+#
+#
+#
+#
+#
+# import sys
+# for line in sys.stdin:
+#     if line=='q\n':
+#         break
+#     print('Input ',line)
+#     # print('Input ',line.rstrip())
+# print("Exit")
+
+
+
+
+
+print("Enter how many characters you want to print")
+n=int(input())
+print("Enter you String")
+x=sys.stdin.readline(n)
+print(f"first {n} characters is {x}\n")
+
+
+#DIFFERENCE
+print("SOA")
+print("ITER")
+
+sys.stdout.write('SOA')
+sys.stdout.write('ITER')
